8e068d5dd53065ad57b5a5448146b34ec520f82e
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-03-14 21:11:16 +0200

    Add user table creation SQL script

---
+++ src/main/resources/db/migration/v1_create_user.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+)
+;
