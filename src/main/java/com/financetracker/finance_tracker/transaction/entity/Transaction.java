7aaef768091156a1e5eaaa9c513a97f298f3c681
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-03-17 16:31:45 +0200

    Implement TransactionService for handling transaction creation and validation

--- src/main/java/com/financetracker/finance_tracker/transaction/entity/Transaction.java
+++ src/main/java/com/financetracker/finance_tracker/transaction/entity/Transaction.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -20,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Transaction {
 
     @Id
